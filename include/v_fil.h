938680b955303665e867baefe75f2afbd0a103e8
Alexinom91 <109924047+Alexinom91@users.noreply.github.com>
2025-03-01 20:03:42 +0100

    costruzione struct principale

---
+++ include/v_fil.h
@@ -0,0 +1,15 @@
+#ifndef V_FIL_H
+#define V_FIL_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+//struttura file 
+typedef struct 
+{
+    int sign;        // Segno del numero: 1 per positivo, -1 per negativo
+    size_t length;   // Lunghezza dell'array di cifre
+    int *digits;     // Array dinamico di cifre
+}fil;
+
+
+#endif
