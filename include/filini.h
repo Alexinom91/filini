c9d95d6e64d6fbbfb9eacd8b5b86d8ef69af6ae9
Alexinom91 <109924047+Alexinom91@users.noreply.github.com>
2025-03-01 22:37:42 +0100

    fatto la struttura per usarla nelle funzioni

---
+++ include/filini.h
@@ -0,0 +1,10 @@
+#ifndef FILINI_H
+#define FILINI_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "v_fil.h"
+typedef _baseFil fil[1];
+
+
+#endif
--- include/v_fil.h
+++ include/v_fil.h
@@ -9,7 +9,7 @@ typedef struct
     int sign;        // Segno del numero: 1 per positivo, -1 per negativo
     size_t length;   // Lunghezza dell'array di cifre
     int *digits;     // Array dinamico di cifre
-}fil;
+}_baseFil;
 
 
 #endif
